bc4dfc515be3d6dc7d6fa06b70fc2f3b5828d94a
alfie-gr <alfie_gray@outlook.com>
2025-06-05 11:31:32 +0100

    Optimized layout for mobile view

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -40,10 +40,10 @@ export default function Home() {
 
                 <div className=' relative flex flex-col md:flex-row space-x-7' >
 
-                    <a href="#_" class="relative inline-flex items-center justify-center p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500">
-                        <span class="absolute inset-0 w-full h-full bg-gradient-to-br from-[#FF8300] via-[#e7864d] to-[#ffffff]"></span>
-                        <span class="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
-                        <span class="relative text-white">Button Text</span>
+                    <a href="#_" class="relative inline-flex items-center justify-center p-4 px-5 py-3 overflow-hidden font-medium text-orange-700 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-orange-400">
+                        <span class="absolute inset-0 w-full h-full bg-gradient-to-br from-orange-600 via-orange-400 to-orange-200"></span>
+                        <span class="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-orange-300 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
+                        <span class="relative text-white">Repair</span>
                     </a>
 
 
