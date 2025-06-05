2ac18707146228405fe5063549fbe4b96314c1d1
alfie-gr <alfie_gray@outlook.com>
2025-06-05 11:40:17 +0100

    Optimized layout for mobile view

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -38,7 +38,7 @@ export default function Home() {
                     <span className="font-semibold font-poppins text-[24px] md:text-[50px] text-[#FF8300] ">Performance you can trust</span>
                 </div>
 
-                <div className=' relative flex flex-col md:flex-row space-x-7' >
+                <div className=' relative flex flex-col space-y-4 md:flex-row md:space-x-7' >
 
 
 
@@ -69,7 +69,7 @@ export default function Home() {
                     >
                         {/* Icon container */}
                         <div className="px-1 flex items-center justify-center z-10">
-                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 mr-2">
+                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 mr-2 text-white">
                                     <path fillRule="evenodd" d="M8.25 6.75a3.75 3.75 0 1 1 7.5 0 3.75 3.75 0 0 1-7.5 0ZM15.75 9.75a3 3 0 1 1 6 0 3 3 0 0 1-6 0ZM2.25 9.75a3 3 0 1 1 6 0 3 3 0 0 1-6 0ZM6.31 15.117A6.745 6.745 0 0 1 12 12a6.745 6.745 0 0 1 6.709 7.498.75.75 0 0 1-.372.568A12.696 12.696 0 0 1 12 21.75c-2.305 0-4.47-.612-6.337-1.684a.75.75 0 0 1-.372-.568 6.787 6.787 0 0 1 1.019-4.38Z" clipRule="evenodd" />
                                     <path d="M5.082 14.254a8.287 8.287 0 0 0-1.308 5.135 9.687 9.687 0 0 1-1.764-.44l-.115-.04a.563.563 0 0 1-.373-.487l-.01-.121a3.75 3.75 0 0 1 3.57-4.047ZM20.226 19.389a8.287 8.287 0 0 0-1.308-5.135 3.75 3.75 0 0 1 3.57 4.047l-.01.121a.563.563 0 0 1-.373.486l-.115.04c-.567.2-1.156.349-1.764.441Z" />
                                 </svg>
@@ -85,20 +85,6 @@ export default function Home() {
                         <span className="relative text-white z-10">Book 1-1 Training</span>
                     </Link>
 
-
-                    <button class="group relative inline-flex h-[calc(48px+8px)]
-                     items-center justify-center rounded-full bg-gradient-to-tr
-                      from-[#FF8300] to-[#ffb15d] py-1 pl-6 pr-14 font-medium
-                       text-neutral-50">
-                        <span class="z-10 pr-2 font-poppins">Book 1-1 Training</span>
-                        <div class="absolute right-1 inline-flex h-12 w-12 items-center
-                         justify-end rounded-full bg-[#e08a2e] transition-[width]
-                          group-hover:w-[calc(100%-8px)]">
-                            <div class=" px-1 flex items-center justify-center">
-                               
-                            </div>
-                        </div>
-                    </button>
 
 
 
