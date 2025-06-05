96f8d87d4ca8728d6f450f485e683cfc5e4c3a5a
alfie-gr <alfie_gray@outlook.com>
2025-06-05 13:40:34 +0100

    Optimized layout for mobile view

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -126,7 +126,7 @@ export default function Home() {
 
 
                     <div className='overflow-x-auto'>
-                        <div className="flex space-x-4 md:space-x-8 px-4 py-2">
+                        <div className="flex space-x-4 md:space-x-12 px-4 py-2">
 
                             {/* Mac */}
                             <div className="flex flex-col items-center justify-between space-y-8 bg-white p-4 rounded-lg transition-transform duration-300 hover:-translate-y-2 hover:scale-105 hover:shadow-lg">
