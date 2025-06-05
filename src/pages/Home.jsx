c2f7a513e4b61999b5945811af7129d577030b0a
alfie-gr <alfie_gray@outlook.com>
2025-06-05 13:52:34 +0100

    Optimized layout for mobile view

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -126,7 +126,7 @@ export default function Home() {
 
 
                     <div className="overflow-x-auto">
-                        <div className="flex gap-x-12 px-4 py-2">
+                        <div className="flex gap-x-24 px-4 py-2">
                             {/* Mac */}
                             <div className="flex flex-col items-center justify-between space-y-8 bg-white p-4 rounded-lg transition-transform duration-300 hover:-translate-y-2 hover:scale-105 hover:shadow-lg min-w-[160px]">
                                 <img src="/assets/mac.png" alt="Mac" className="h-[200px] object-contain" />
